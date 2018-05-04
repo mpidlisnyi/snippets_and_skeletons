faf704a9c02027b3fe71ca5f73f88ebcc3667652
Maksym Pidlisnyi <maksim@nightbook.info>
2018-05-04 10:00:49 +0300

    python package

---
+++ python/package/setup.py
@@ -0,0 +1,25 @@
+# just replace mypackage
+from pip.req import parse_requirements as pr
+from setuptools import setup, find_packages
+
+ins_req = [str(ir.req) for ir in pr('requirements.txt', session=False)]
+
+
+def main():
+    setup(
+        name='mypackage',
+        version_format='{tag}.{commitcount}+{gitsha}',
+        setup_requires=['setuptools-git-version'],
+        packages=find_packages(),
+        long_description='mypackage',
+        install_requires=ins_req,
+        include_package_data=True,
+        entry_points='''
+            [console_scripts]
+            mypackage=mypackage.script:main
+        ''',
+    )
+
+
+if __name__ == '__main__':
+    main()
